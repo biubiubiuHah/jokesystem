23afbb5cbbb12d4ca9b8b87a1eca241f96a693d7
Yannyezixin <646840080@qq.com>
2014-01-03 18:51:33 +0800

    改了首页后缀

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/admin/includes/db.inc.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/admin//includes/html.inc.php';
+
+if(isset($_POST['zan']))
+{
+	$num = htmlout($joke['zan']);
+	$num += 1;
+	try
+	{
+		$sql = 'UPDATE joke SET zan = :zan WHERE id = :id';
+		$s = $pdo -> prepare($sql);
+		$s -> bindValue(':zan',$num);
+		$s -> bindValue(':id',$_POST['id']);
+		$s -> execute();
+	}
+	catch(PDOException $e)
+	{
+		$output = 'error to zan this joke';
+		include $_SERVER['DOCUMENT_ROOT'].'/admin/includes/output.html.php';
+		exit();
+	}
+	header('Location: .');
+	exit();
+}
+
+try
+{
+	$sql = 'SELECT joke.id,joketext,name,email,zan FROM joke INNER JOIN author ON authorid = author.id ';
+	$result = $pdo -> query($sql);
+}
+catch(PDOException $e)
+{
+	$output = "error fetching jokes".$e -> getMessage();
+	include $_SERVER['DOCUMENT_ROOT'].'/admin/includes/output.html.php';
+	exit();
+}
+foreach($result as $row)
+{
+	$jokes[] = array('id' => $row['id'],'text' => $row['joketext'],'name' => $row['name'],
+		'email' => $row['email'],'zan' => $row['zan']);
+}
+
+
+
+include 'jokes.html.php';
---
+++ jokes.html.php
@@ -0,0 +1,32 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/admin//includes/html.inc.php'; ?>
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <meta charset = "utf-8">
+    <title>Joke</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <style type="text/css">
+  body {
+	background-color: #CCC;
+}
+  </style>
+  </head>
+  <body>
+  
+  <hr align="center" width="70%">   
+  <table width="70%" border="1" align="center" cellpadding="0" cellspacing="2">
+    <tr>
+      <th colspan="2"><strong>Joke ~welcome to you to come here!</strong></th>
+    </tr>
+    <?php foreach($jokes as $joke): ?>
+    <tr>
+      <td><?php htmlout($joke['text']); ?></td>
+      <td>By:<a href="mailto:<?php htmlout($joke['email']); ?>"><?php htmlout($joke['name']); ?></a></td>
+      <td><form action ="?" method = 'post'>
+        <input type = "hidden" name = "id" value = "<?php htmlout($joke['id']); ?>">
+        <input type = "submit" name = "zan" value = "赞" >+<?php htmlout($joke['zan'])?></form></td>
+    </tr>
+  <?php endforeach; ?>
+  </table>
+  </body>
+</html>
